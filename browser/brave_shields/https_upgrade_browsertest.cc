99c33b11ef671235447c8b6bb00b72c96a22c591
Arthur Edelstein <aedelstein@brave.com>
2022-12-05 10:33:00 -0800

    fix gn_check failures

--- browser/brave_shields/https_upgrade_browsertest.cc
+++ browser/brave_shields/https_upgrade_browsertest.cc
@@ -18,7 +18,6 @@
 #include "components/prefs/pref_service.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
-#include "content/public/test/content_browser_test_utils.h"
 #include "content/public/test/content_mock_cert_verifier.h"
 #include "content/public/test/test_navigation_observer.h"
 #include "net/cert/x509_certificate.h"
