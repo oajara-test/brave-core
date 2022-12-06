ef248e41f4d7b05210e895a445f7ec07d3b56058
sangwoo.ko <sangwoo108@gmail.com>
2022-12-06 17:06:03 +0900

    Fix eslint error

--- components/playlist/resources/actions/playlist_action_creators.ts
+++ components/playlist/resources/actions/playlist_action_creators.ts
@@ -3,12 +3,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-import {Playlist} from 'components/definitions/playlist'
+import { Playlist } from 'components/definitions/playlist'
 import * as PlaylistMojo from 'gen/brave/components/playlist/mojom/playlist.mojom.m.js'
-import {action} from 'typesafe-actions'
+import { action } from 'typesafe-actions'
 
 // Constants
-import {types} from '../constants/playlist_types'
+import { types } from '../constants/playlist_types'
 
 export const playlistLoaded = (playlists: PlaylistMojo.Playlist[]) =>
     action(types.PLAYLIST_LOADED, playlists)
